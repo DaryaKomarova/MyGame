77649fab2364ecb39e5f1de8619a0bab20c224a4
DaryaKomarova <darakomarova629@gmail.com>
2024-10-01 12:06:55 +0300

    Add GameType

--- Сахар/Form1.cs
+++ Сахар/Form1.cs
@@ -2,9 +2,27 @@ namespace Сахар
 {
     public partial class Form1 : Form
     {
+        private GameType gameType;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameType = GameType.PvsC;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameType = GameType.PvsP;
+        }
+    }
+
+    public enum GameType
+    {
+        PvsP,
+        PvsC
     }
 }
