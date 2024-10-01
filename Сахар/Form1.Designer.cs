940515476c4627dd04233e6b20c8d36c3cf763e0
OlesyaGushina <olgushina10@gmail.com>
2024-10-01 13:00:05 +0400

    Update Form1.Designer.cs

--- Сахар/Form1.Designer.cs
+++ Сахар/Form1.Designer.cs
@@ -34,29 +34,32 @@
             // 
             // button1
             // 
-            button1.Location = new Point(189, 205);
+            button1.Location = new Point(627, 544);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(150, 57);
+            button1.Size = new Size(120, 46);
             button1.TabIndex = 0;
             button1.Text = "Один игрок";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(412, 205);
+            button2.Location = new Point(760, 544);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(141, 57);
+            button2.Size = new Size(113, 46);
             button2.TabIndex = 1;
             button2.Text = "Два игрока";
             button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(884, 601);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
