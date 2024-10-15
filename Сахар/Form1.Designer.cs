37c9fa2066d3d34ce6b67f7d1a6d6fd02b375ebe
DaryaKomarova <darakomarova629@gmail.com>
2024-10-15 09:49:11 +0300

    Update Form1.Designer.cs

--- Сахар/Form1.Designer.cs
+++ Сахар/Form1.Designer.cs
@@ -64,102 +64,119 @@
             // 
             // _1_2
             // 
+            _1_2.BackColor = SystemColors.Window;
+            _1_2.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _1_2.Location = new Point(310, 111);
             _1_2.Name = "_1_2";
             _1_2.Size = new Size(126, 103);
             _1_2.TabIndex = 2;
-            _1_2.Text = "button3";
-            _1_2.UseVisualStyleBackColor = true;
+            _1_2.Text = "X";
+            _1_2.UseVisualStyleBackColor = false;
             _1_2.Click += _1_2_Click;
             // 
             // _1_1
             // 
+            _1_1.BackColor = SystemColors.Window;
+            _1_1.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _1_1.Location = new Point(178, 111);
             _1_1.Name = "_1_1";
             _1_1.Size = new Size(126, 103);
             _1_1.TabIndex = 3;
-            _1_1.Text = "button4";
-            _1_1.UseVisualStyleBackColor = true;
+            _1_1.Text = "X";
+            _1_1.UseVisualStyleBackColor = false;
             _1_1.Click += button4_Click;
             // 
             // _1_3
             // 
+            _1_3.BackColor = SystemColors.Window;
+            _1_3.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _1_3.Location = new Point(442, 111);
             _1_3.Name = "_1_3";
             _1_3.Size = new Size(126, 103);
             _1_3.TabIndex = 4;
-            _1_3.Text = "button5";
-            _1_3.UseVisualStyleBackColor = true;
+            _1_3.Text = "X";
+            _1_3.UseVisualStyleBackColor = false;
             _1_3.Click += _1_3_Click;
             // 
             // _2_1
             // 
+            _2_1.BackColor = SystemColors.Window;
+            _2_1.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _2_1.Location = new Point(178, 220);
             _2_1.Name = "_2_1";
             _2_1.Size = new Size(126, 103);
             _2_1.TabIndex = 5;
-            _2_1.Text = "button6";
-            _2_1.UseVisualStyleBackColor = true;
+            _2_1.Text = "X";
+            _2_1.UseVisualStyleBackColor = false;
             _2_1.Click += _2_1_Click;
             // 
             // _2_2
             // 
+            _2_2.BackColor = SystemColors.Window;
+            _2_2.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _2_2.Location = new Point(310, 220);
             _2_2.Name = "_2_2";
             _2_2.Size = new Size(126, 103);
             _2_2.TabIndex = 6;
-            _2_2.Text = "button7";
-            _2_2.UseVisualStyleBackColor = true;
+            _2_2.Text = "X";
+            _2_2.UseVisualStyleBackColor = false;
             _2_2.Click += _2_2_Click;
             // 
             // _2_3
             // 
+            _2_3.BackColor = SystemColors.Window;
+            _2_3.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _2_3.Location = new Point(442, 220);
             _2_3.Name = "_2_3";
             _2_3.Size = new Size(126, 103);
             _2_3.TabIndex = 7;
-            _2_3.Text = "button8";
-            _2_3.UseVisualStyleBackColor = true;
+            _2_3.Text = "X";
+            _2_3.UseVisualStyleBackColor = false;
             _2_3.Click += _2_3_Click;
             // 
             // _3_1
             // 
+            _3_1.BackColor = SystemColors.Window;
+            _3_1.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _3_1.Location = new Point(178, 329);
             _3_1.Name = "_3_1";
             _3_1.Size = new Size(126, 103);
             _3_1.TabIndex = 8;
-            _3_1.Text = "button9";
-            _3_1.UseVisualStyleBackColor = true;
+            _3_1.Text = "X";
+            _3_1.UseVisualStyleBackColor = false;
             _3_1.Click += _3_1_Click;
             // 
             // _3_2
             // 
+            _3_2.BackColor = SystemColors.Window;
+            _3_2.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _3_2.Location = new Point(310, 329);
             _3_2.Name = "_3_2";
             _3_2.Size = new Size(126, 103);
             _3_2.TabIndex = 9;
-            _3_2.UseVisualStyleBackColor = true;
+            _3_2.Text = "X";
+            _3_2.UseVisualStyleBackColor = false;
             _3_2.Click += _3_2_Click;
             // 
             // _3_3
             // 
+            _3_3.BackColor = SystemColors.Window;
+            _3_3.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point);
             _3_3.Location = new Point(442, 329);
             _3_3.Name = "_3_3";
             _3_3.Size = new Size(126, 103);
             _3_3.TabIndex = 10;
-            _3_3.Text = "button11";
-            _3_3.UseVisualStyleBackColor = true;
+            _3_3.Text = "X";
+            _3_3.UseVisualStyleBackColor = false;
             _3_3.Click += _3_3_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Location = new Point(338, 42);
-            label1.Name = "";
-            label1.Size = new Size(59, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 25);
             label1.TabIndex = 11;
-            label1.Text = "";
-
             // 
             // Form1
             // 
